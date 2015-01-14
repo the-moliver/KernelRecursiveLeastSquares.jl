8369b584633517caec7ed033b1c0279f0ceecc79
Michael Oliver <michael.d.oliver@gmail.com>
2015-01-13 17:27:48 -0800

    fix up test code

--- src/krls_test.jl
+++ src/krls_test.jl
@@ -1,3 +1,5 @@
+using KernelLibrary, KernelRecursiveLeastSquares
+
 x = randn(5000,20);
 h = randn(20,1);
 h2 = randn(20,1);
@@ -7,11 +9,11 @@ x2 = randn(200,20);
 y2 = (x2*h + 1).^3 + 4*(x2*h2 + 1).^2;
 
 
-kernelfunc(x,y) = polynomial_kernel(x,y,2);
+k(x,y) = polynomial_kernel(x,y,2);
 
-(alpha1, dict1, Kinv1, dict1_idx) = krls(x, y, kernelfunc)
+(alpha1, dict1, Kinv1, dict1_idx) = krls(x', y', kernelfunc=k, nu=1., maxdict=300);
 
-ypred = ((dict1*x2' + 1) .^ 2)'*alpha1;
+ypred = alpha1'*k(dict1, x2');
 
-cor(ypred, y2)
+cor(ypred', y2)
 
