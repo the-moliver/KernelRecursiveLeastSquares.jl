a3dd19cd7c353ed328c1eb533c64b365d5fbaea3
Michael Oliver <michael.d.oliver@gmail.com>
2015-01-13 17:17:42 -0800

    fix typo

--- src/krls.jl
+++ src/krls.jl
@@ -90,7 +90,7 @@ for ii = idx[2:end];
 		alpha = alpha + Kinv*qt*(y[ii] - kt'*alpha);
 
 		if mod(m2,50)==0
-			println("On sample: $ms of $(sz[2])\n")
+			println("On sample: $m2 of $(sz[2])\n")
 		end
 
 	end
