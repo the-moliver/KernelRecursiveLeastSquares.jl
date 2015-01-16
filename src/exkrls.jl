f915a331ac17cb805eae36e11a80ba456d0355df
Michael Oliver <michael.d.oliver@gmail.com>
2015-01-15 20:38:56 -0800

    extended krls

--- src/exkrls.jl
+++ src/exkrls.jl
@@ -1,14 +1,14 @@
-function krls{T}(x::Array{T}, y::Array{T}; nu=1., lambda=0.1, kernelfunc=linear_kernel, maxdict=100, index="lin")
+function exkrls{T}(x::Array{T}, y::Array{T}; nu=1., α=.5, β=0.5, q=.1, λ=0.1, kernelfunc=linear_kernel, maxdict=100)
 
 # krls(x, y)
 #
-# Julia version of the Kernel Recursive Least Squares Algorithm
+# Julia version of the Extended Kernel Recursive Least Squares Algorithm
 #
 # Input:
 # 			  x : d(dimension) x N(samples) array
 # 			  y : 1 x N(samples) vector
 # 	         nu : approximate linear dependency (ALD) threshold, controls sparsity (default: 1.)
-# 	     lambda : regularization parameter (default: 0.1)
+# 	     λ : regularization parameter (default: 0.1)
 #        kernel : type of kernel (default : 'linear_kernel')
 #       maxdict : maximum dictionary size (default: 100)
 #         index : how to cycle through samples, 'lin' goes in order, 'rand' goes randomly, or you can provide your own vector index (default: 'lin')
@@ -19,48 +19,48 @@ function krls{T}(x::Array{T}, y::Array{T}; nu=1., lambda=0.1, kernelfunc=linear_
 #		   Kinv : Inverse of kernel matrix
 #	   dict_idx : dictionary sample indicies
 #
+#
+# Citation:
+# Y. Engel, S. Mannor, and R. Meir, “The kernel recursive least-squares algorithm,” IEEE Transactions on Signal Processing, vol. 52, no. 8, pp. 2275–2285, 2004.
 
 
-
-lambda = convert(T, lambda)
+λ = convert(T, λ)
 nu = convert(T, nu)
 
-lambda2 = lambda.^2
+λ2 = λ.^2
 
 sz = size(x)
 
 dict = zeros(eltype(x), sz[1], maxdict)
 dict_idx = zeros(eltype(x), maxdict, 1)
-## Set order to look at points
-if index == "rand"
-	idx = randperm(sz[2])
-elseif index == "lin"
-	idx = 1:sz[2];
-else
-	idx = index;
-end
+
 
 ## Initialize
-K = kernelfunc(x[:,idx[1]], x[:,idx[1]]) + lambda2
+K = kernelfunc(x[:,1], x[:,1])
 Kinv = (1./K)'
-alpha = (y[idx[1]]./K)'
-dict[:,1] = x[:,idx[1]]
-dict_idx[1] = idx[1]
-P = 1
+alpha = (α*y[1] ./ (λ*β+K))'
+pr = λ*β / (abs(α)^2)
+dict[:,1] = x[:,1]
+dict_idx[1] = 1
+P = one(eltype(x))
 m = 1
 m2 = 1
 
-for ii = idx[2:end]
+for ii = 2:length(y)
 
 	m2 = m2 + 1
 
-	kt = kernelfunc(dict[:,1:m], x[:,ii]) + lambda2
+	kt = kernelfunc(dict[:,1:m], x[:,ii]) + λ2
 
-	ktt = kernelfunc(x[:,ii], x[:,ii]) + lambda2
+	ktt = kernelfunc(x[:,ii], x[:,ii]) + λ2
 
 	at = Kinv * kt'
 
 	dt = ktt - kt*at
+
+	kta = kt*alpha
+
+	e = y[ii] - kta[1]
 
 	if abs(dt[1]) > nu && m < maxdict
 
@@ -73,7 +73,7 @@ for ii = idx[2:end]
 
 		P = [P zeros(eltype(x),size(P,1), 1); zeros(eltype(x),1, size(P,1)) 1]
 
-		alpha = [alpha - ((at ./ dt) * (y[ii] - kt*alpha)); (1./dt)*(y[ii] - kt*alpha)]
+		alpha = [alpha - ((at ./ dt) * e); (1./dt)*e]
 
 		if mod(m,50)==0
 			println("Dictionary Size: $m of $maxdict")
@@ -94,11 +94,8 @@ for ii = idx[2:end]
 
 		# alpha +=  Kinv*qt*(y[ii] - kt*alpha)
 		# optimized as
-		kta = kt*alpha
 
-		dif = y[ii] - kta[1]
-
-		Base.LinAlg.BLAS.gemm!('N', 'N', dif, Kinv,qt, one(eltype(y)), alpha)
+		Base.LinAlg.BLAS.gemm!('N', 'N', e, Kinv,qt, one(eltype(y)), alpha)
 
 		if mod(m2,50)==0
 			println("On sample: $m2 of $(sz[2])")
